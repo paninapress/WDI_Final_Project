2b96259e1afe8a27f5384ee878a20da018688584
melodicodyssey <stevedbauer@gmail.com>
2014-03-22 23:35:20 -0700

    replaced Angular files and now have working angular example on index.html.erb

---
+++ app/assets/javascripts/acquaintly/acquaintly.js
@@ -0,0 +1,9 @@
+var AcquaintlyApp = angular.module('AcquaintlyApp', []);
+
+AcquaintlyApp.controller('AcquaintlyController', function($scope) {
+
+  $scope.testName = "Spartacus";
+
+  $scope.testList = [1,2,3,4,5];
+
+});
