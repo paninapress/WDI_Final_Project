82932247808372822d2cf54cd6ef4cd32a70b1c3
melodicodyssey <stevedbauer@gmail.com>
2014-03-23 21:32:45 -0700

    added 'category' column to 'connection' table in DB

--- db/migrate/20140324000847_create_connections.rb
+++ db/migrate/20140324000847_create_connections.rb
@@ -5,7 +5,7 @@ class CreateConnections < ActiveRecord::Migration
       t.belongs_to :contact, index: true
       t.belongs_to :first_name, index: true
       t.belongs_to :last_name, index: true
-
+      t.char :category
       t.timestamps
     end
   end
