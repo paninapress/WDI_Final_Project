bf8e7f83dc11a1ab2180f8013fe6ed2518c0fdc8
melodicodyssey <stevedbauer@gmail.com>
2014-03-25 13:23:45 -0700

    Defined callback route and rails controller to return omniauth JSON

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,22 @@
 class SiteController < ApplicationController
-
+  # respond_to :json
   def index
+  end
+
+  def omniauth
+    provider = params['provider']
+    # call method from user.rb that will return the parsed information
+    # from the response that we should already have when this controller method is called
+    # and wrap that up in JSON for the Angular controller to collect via $http
+    # User.get_info (provider, request.env['omniauth.auth']
+    auth = request.env['omniauth.auth']
+    binding.pry
+    # respond_to do |f|
+    #   f.html
+    #   f.json {render :json => auth}
+    # end
+    # respond_with auth
+    render :json => auth
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ WdiFinal::Application.routes.draw do
 
   root to: 'site#index'
   resources :site
+  get '/auth/:provider/callback', to: 'site#omniauth'
 
 end
