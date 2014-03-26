4803de0406d3c160299b2d8dca3bf20d7b7f5230
melodicodyssey <stevedbauer@gmail.com>
2014-03-25 17:04:42 -0700

    added 'access_token' and 'expires_at' columns to users table in DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325231940) do
+ActiveRecord::Schema.define(version: 20140325234705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20140325231940) do
     t.integer  "last_name_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "access_token"
+    t.integer  "expires_at"
   end
 
   add_index "users", ["first_name_id"], name: "index_users_on_first_name_id", using: :btree
