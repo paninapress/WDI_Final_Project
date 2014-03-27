ea050ad56d6191f7aeeece2eb3dded1f38156f33
paninapress <NPacifico25@gmail.com>
2014-03-27 11:54:09 -0700

    custom routes for site

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ WdiFinal::Application.routes.draw do
 
   devise_for :users
   root to: 'site#index'
-  resources :site
+  
+  get '/dashboard', to: 'site#show'
   get '/auth/:provider/callback', to: 'site#omniauth'
 
 end
