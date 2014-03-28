778cd5b61935c1bbb738f317218fe277708b840d
melodicodyssey <stevedbauer@gmail.com>
2014-03-27 20:11:08 -0700

    experimenting with category view/controller

--- app/assets/javascripts/acquaintly/acquaintly_controller.js
+++ app/assets/javascripts/acquaintly/acquaintly_controller.js
@@ -15,7 +15,33 @@ AppController.controller("AppCtrl",['$scope','$location','$anchorScroll', '$reso
     $scope.go = function(path){
       $location.path(path);
     };
+
     Connection = $resource('/connections/:id', {id: "@id"});
+
     $scope.connections = Connection.query();
 
+    $scope.toBeCategorized = function(){
+
+      for (i in $scope.connections) {
+        noCategory = [];
+        if ($scope.connections[i]['category'] === null || $scope.connections[i]['category'] === 0) {
+          noCategory.push($scope.connections[i]);
+        }
+        noCategory[0]['category'] = 0;
+        return noCategory;
+      };
+    };
+
+    $scope.categorized = function(id, category) {
+      // $scope.toBeCategorized[id]['category'] = category;
+      // find the id of $scope.connections we're looking for
+      // make custom route to custom method in ConnectionsController to update
+      $http.put("/category/"+id+"/"+category);
+    }
+
   }]);
+
+// ng-show if category === 0
+// upon iteration, category is set to 0
+// if "Slip" is clicked, category goes to 5
+// else, category is set to 1..4
