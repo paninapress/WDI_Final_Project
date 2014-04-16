f6f16d56261493c81f31a3fe1a1a3bd7a635b6d7
melodicodyssey <stevedbauer@gmail.com>
2014-04-15 17:39:47 -0700

    removed require for params[:comment] in logs#create

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,7 +3,7 @@ class LogsController < ApplicationController
     id = params[:connection_id]
     user = current_user
     connection = Connection.find(id)
-    log = Log.create(source: params.require(:source), comment: params.require(:comment), timestamp: Time.now)
+    log = Log.create(source: params.require(:source), comment: params[:comment], timestamp: Time.now)
     connection.logs << log
     updated = Connection.get_connection(user, connection)
     render :json => {response: updated}
