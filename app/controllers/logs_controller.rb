8022abf720ac5c18618feac5c51820626fef6f80
melodicodyssey <stevedbauer@gmail.com>
2014-04-17 16:41:22 -0700

    syntax edit for timestamp Date in rails

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,7 +3,8 @@ class LogsController < ApplicationController
     id = params[:connection_id]
     user = current_user
     connection = Connection.find(id)
-    date = params[:log][:date] != "" ? Date.parse(params.require(:log)[:date]) : Date.today
+    # date = params[:log][:date] != "" ? Date.parse(params.require(:log)[:date]) : Date.today
+    date = params[:log][:date] != "" ? params[:log][:date] : Date.today
     log = Log.create(source: params.require(:log)[:source], comment: params[:log][:comment], timestamp: date)
     connection.logs << log
     updated = Connection.get_connection(user, connection)
