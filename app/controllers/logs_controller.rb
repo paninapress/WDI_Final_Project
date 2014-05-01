fe6a1c065a42a1b79b6de24c26b1a14bc216751b
melodicodyssey <stevedbauer@gmail.com>
2014-05-01 13:28:43 -0700

    abstracted logs_controller method to log.rb

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,16 +1,9 @@
 class LogsController < ApplicationController
   def create
-    id = params[:connection_id]
     user = current_user
-    connection = Connection.find(id)
-    data = {source: params.require(:log)[:source],
-            comment: params[:log][:comment] != "" ? params[:log][:comment] : "n/a",
-            timestamp: params[:log][:date] != "" ? params[:log][:date] : Date.today
-            }
-    log = Log.create(data)
-    connection.logs << log
-    updated = Connection.get_connection(user, connection)
-    render :json => {response: updated}
+    connection = Connection.find(params[:connection_id])
+    log = Log.create_log(connection, {source: params.require(:log)[:source], comment: params[:log][:comment] != "" ? params[:log][:comment] : "n/a", timestamp: params[:log][:date] != "" ? params[:log][:date] : Date.today})
+    render :json => {response: log}
   end
 
   def destroy
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,14 @@
 class Log < ActiveRecord::Base
   belongs_to :connection
+
+  def create_log connection, data
+    log = Log.create(data)
+    connection.logs << log
+    last_date = log
+    connection.logs.each {|log| last_date = log if log.timestamp > last_date.timestamp}
+    if (connection.category != 0 && connection.category != nil) && !last_date.nil?
+      connection.health = ((Date.today - last_date.timestamp) / connection.category).to_f
+    end
+    connection
+  end
 end
