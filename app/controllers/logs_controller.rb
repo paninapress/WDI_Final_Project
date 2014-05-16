c46168e5888e237d558c1c542c80e3e90c7c9602
melodicodyssey <stevedbauer@gmail.com>
2014-05-15 18:09:01 -0700

    updated LogsCtrl to account for empty date

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,7 @@
 class LogsController < ApplicationController
   def create
     connection = Connection.find(params[:connection_id])
-    Log.create_log(connection, {source: params[:log][:source], comment: params[:log][:comment] != "" ? params[:log][:comment] : "n/a", timestamp: params[:log][:date] ? params[:log][:date] : Date.today})
+    Log.create_log(connection, {source: params[:log][:source], comment: params[:log][:comment] != "" ? params[:log][:comment] : "n/a", timestamp: params[:log][:date] && params[:log][:date].length > 0 ? params[:log][:date] : Date.today})
     render :json => connection.as_json(:include => {:logs => {:only => [:id, :source, :comment, :timestamp]}})
   end
 
