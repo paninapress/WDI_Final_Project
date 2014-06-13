fdf44a05a085487e79da3a1157fe2bac24d960a3
paninapress <NPacifico25@gmail.com>
2014-06-13 15:48:15 -0700

    if user is signed in site will go to dashboard

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,9 @@ class SiteController < ApplicationController
   before_filter :authenticate_user!, except: :index
 
   def index
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
   end
 
   def show
