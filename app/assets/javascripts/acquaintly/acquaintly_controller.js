3f43dab272506016082c935c42cfd10323715ec0
paninapress <npacifico25@gmail.com>
2015-01-30 13:53:52 -0800

    removed gradient colors and fixed categorize btn from not showing when new contacts were added

--- app/assets/javascripts/acquaintly/acquaintly_controller.js
+++ app/assets/javascripts/acquaintly/acquaintly_controller.js
@@ -80,6 +80,9 @@ AppController.controller("AppCtrl",["$scope","$location","$anchorScroll", "$reso
     if (sum == 0 && total != 0){
       $('#categorize-btn').addClass('hide');
     }
+    else{
+      $('#categorize-btn').removeClass('hide');
+    }
     return sum + " / " + total;
   }
   $scope.categorized = function(contact, cat, list) {
@@ -147,174 +150,19 @@ AppController.controller("AppCtrl",["$scope","$location","$anchorScroll", "$reso
       return "none";
     }
   };
-  var gradientColors = {80: 'rgb(69,195,0)', 
-                        81: 'rgb(77,197,2)',
-                        82: 'rgb(84,199,4)',
-                        83: 'rgb(94,201,6)',
-                        84: 'rgb(102,202,8)',
-                        85: 'rgb(110,204,10)',
-                        86: 'rgb(112,206,12)',
-                        87: 'rgb(129,208,14)',
-                        88: 'rgb(137,210,16)',
-                        89: 'rgb(144,211,18)',
-                        90: 'rgb(156,214,20)',
-                        91: 'rgb(162,215,22)',
-                        92: 'rgb(170,217,24)',
-                        93: 'rgb(179,219,26)',
-                        94: 'rgb(189,221,28)',
-                        95: 'rgb(194,222,30)',
-                        96: 'rgb(202,224,32)',
-                        97: 'rgb(213,226,34)',
-                        98: 'rgb(223,228,36)',
-                        99: 'rgb(232,230,38)',
-                        100: 'rgb(236,231,40)',
-                        101: 'rgb(221,228,48)',
-                        102: 'rgb(209,226,59)',
-                        103: 'rgb(196,224,71)',
-                        104: 'rgb(184,222,83)',
-                        105: 'rgb(173,220,92)',
-                        106: 'rgb(162,218,102)',
-                        107: 'rgb(151,216,114)',
-                        108: 'rgb(138,214,126)',
-                        109: 'rgb(127,212,135)',
-                        110: 'rgb(117,210,145)',
-                        111: 'rgb(103,208, 159)',
-                        112: 'rgb(93,206,16)',
-                        113: 'rgb(83,204,176)',
-                        114: 'rgb(71,202,189)',
-                        115: 'rgb(59,200,200)',
-                        116: 'rgb(48,198,210)',
-                        117: 'rgb(36,196,222)',
-                        118: 'rgb(24,194,232)',
-                        119: 'rgb(13,192,242)',
-                        120: 'rgb(0,190,255)'};
   // Calculate div style based on health status
   $scope.changeStyle = function(health){
     if (health === null) {
       return "{background: 'white'}";
     }
-    else if (health <= 0.8){
-      return "{background: '"+ gradientColors[80]+ "'}";
-    }
-    else if (health <= 0.81){
-      return "{background: '"+ gradientColors[81]+ "'}";
-    }
-    else if (health <= 0.82){
-      return "{background: '"+ gradientColors[82]+ "'}";
-    }
-    else if (health <= 0.83){
-      return "{background: '"+ gradientColors[83]+ "'}";
-    }
-    else if (health <= 0.84){
-      return "{background: '"+ gradientColors[84]+ "'}";
-    }
-    else if (health <= 0.85){
-      return "{background: '"+ gradientColors[85]+ "'}";
-    }
-    else if (health <= 0.86){
-      return "{background: '"+ gradientColors[86]+ "'}";
-    }
-    else if (health <= 0.87){
-      return "{background: '"+ gradientColors[87]+ "'}";
-    }
-    else if (health <= 0.88){
-      return "{background: '"+ gradientColors[88]+ "'}";
-    }
-    else if (health <= 0.89){
-      return "{background: '"+ gradientColors[89]+ "'}";
-    }
-    else if (health <= 0.90){
-      return "{background: '"+ gradientColors[90]+ "'}";
-    }
-    else if (health <= 0.91){
-      return "{background: '"+ gradientColors[91]+ "'}";
-    }
-    else if (health <= 0.92){
-      return "{background: '"+ gradientColors[92]+ "'}";
-    }
-    else if (health <= 0.93){
-      return "{background: '"+ gradientColors[93]+ "'}";
-    }
-    else if (health <= 0.94){
-      return "{background: '"+ gradientColors[94]+ "'}";
-    }
-    else if (health <= 0.95){
-      return "{background: '"+ gradientColors[95]+ "'}";
-    }
-    else if (health <= 0.96){
-      return "{background: '"+ gradientColors[96]+ "'}";
-    }
-    else if (health <= 0.97){
-      return "{background: '"+ gradientColors[97]+ "'}";
-    }
-    else if (health <= 0.98){
-      return "{background: '"+ gradientColors[98]+ "'}";
-    }
-    else if (health <= 0.99){
-      return "{background: '"+ gradientColors[99]+ "'}";
+    else if (health < 0.8){
+      return "{background: '#89E818'}";
     }
     else if (health <= 1){
-      return "{background: '"+ gradientColors[100]+ "'}";
+      return "{background: '#FFEB2C'}";
     }
-    else if (health <= 1.01){
-      return "{background: '"+ gradientColors[101]+ "'}";
-    }
-    else if (health <= 1.02){
-      return "{background: '"+ gradientColors[102]+ "'}";
-    }
-    else if (health <= 1.03){
-      return "{background: '"+ gradientColors[103]+ "'}";
-    }
-    else if (health <= 1.04){
-      return "{background: '"+ gradientColors[104]+ "'}";
-    }
-    else if (health <= 1.05){
-      return "{background: '"+ gradientColors[105]+ "'}";
-    }
-    else if (health <= 1.06){
-      return "{background: '"+ gradientColors[106]+ "'}";
-    }
-    else if (health <= 1.07){
-      return "{background: '"+ gradientColors[107]+ "'}";
-    }
-    else if (health <= 1.08){
-      return "{background: '"+ gradientColors[108]+ "'}";
-    }
-    else if (health <= 1.09){
-      return "{background: '"+ gradientColors[109]+ "'}";
-    }
-    else if (health <= 1.10){
-      return "{background: '"+ gradientColors[110]+ "'}";
-    }
-    else if (health <= 1.11){
-      return "{background: '"+ gradientColors[111]+ "'}";
-    }
-    else if (health <= 1.12){
-      return "{background: '"+ gradientColors[112]+ "'}";
-    }
-    else if (health <= 1.13){
-      return "{background: '"+ gradientColors[113]+ "'}";
-    }
-    else if (health <= 1.14){
-      return "{background: '"+ gradientColors[114]+ "'}";
-    }
-    else if (health <= 1.11){
-      return "{background: '"+ gradientColors[115]+ "'}";
-    }
-    else if (health <= 1.16){
-      return "{background: '"+ gradientColors[116]+ "'}";
-    }
-    else if (health <= 1.17){
-      return "{background: '"+ gradientColors[117]+ "'}";
-    }
-    else if (health <= 1.18){
-      return "{background: '"+ gradientColors[118]+ "'}";
-    }
-    else if (health <= 1.19){
-      return "{background: '"+ gradientColors[119]+ "'}";
-    }
-    else if (health > 1.19 && health <= 3){
-      return "{background: '"+ gradientColors[120]+ "'}";
+    else if (health > 1 && health < 3){
+      return "{background: '#4FB2FF'}";
     }
     else if (health >= 3){
       return "{background: 'lightgrey'}";
